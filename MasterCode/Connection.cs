a3525fed04a069e604149ceeada62836d4b0abda
htscott98 <htscott98@hotmail.com>
2022-05-05 14:12:33 -0400

    Add project files.

---
+++ MasterCode/Connection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterCode
+{
+    public class Connection
+    {
+
+        public static SqlConnection InitServerConnection()
+        {
+            string connectionString = "Server=localhost; Trusted_Connection=True";
+            return new SqlConnection(connectionString);
+        }
+
+        public static SqlConnection InitPRDConnection()
+        {
+            string connectionString = "Server=localhost; Database=PRD; Trusted_Connection=True;";
+            return new SqlConnection(connectionString);
+        }
+
+
+    }
+}
---
+++ MasterCode/Log.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterCode
+{
+    public class Log
+    {
+
+        public static void InsertMasterLogging(params (string ID, string Value)[] pairs)
+        {
+            foreach (var pair in pairs)
+            {
+                using (var cnn = Connection.InitPRDConnection())
+                {
+                    Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+
+                    keyValuePairs.Add("@DevConsoleMasterID", pair.ID);
+                    keyValuePairs.Add("@Value", pair.Value);
+                    keyValuePairs.Add("@Timestamp", DateTime.Now.ToString());
+
+                    string sqlStatement = "INSERT INTO DevConsoleMasterLogs(DevConsoleMasterID, Value, Timestamp) VALUES (@DevConsoleMasterID, @Value, @Timestamp)";
+
+                    Execute.ExecuteStatementReturnBool(Connection.InitPRDConnection(), sqlStatement, keyValuePairs);
+
+
+                }
+            }
+        }
+
+    }
+}
